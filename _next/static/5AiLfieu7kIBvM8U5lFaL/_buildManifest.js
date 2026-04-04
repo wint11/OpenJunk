94a94fe8d1e7572d230eb04defd33f1ec27cfb4b
wint11 <1811879068@qq.com>
2026-04-04 15:32:35 +0800

    Fix basePath and assetPrefix for /OpenJunk subpath

--- _next/static/5AiLfieu7kIBvM8U5lFaL/_buildManifest.js
+++ _next/static/5AiLfieu7kIBvM8U5lFaL/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/OpenJunk//_next/:path+",
+        "destination": "/OpenJunk/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
